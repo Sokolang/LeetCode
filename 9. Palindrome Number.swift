98b30afc8781a3a19e9147ba101d1b61d27c99c2
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2022-12-02 16:57:03 +0200

    Add files via upload

---
+++ 9. Palindrome Number.swift
@@ -0,0 +1,35 @@
+//
+//  9. Palindrome Number.swift
+//  
+//
+//  Created by Anzhelika Sokolova on 02.12.2022.
+//
+
+/*
+ 9. Palindrome Number
+ 
+ Given an integer x, return true if x is a
+ palindrome, and false otherwise.
+
+ Example 1:
+
+ Input: x = 121
+ Output: true
+ Explanation: 121 reads as 121 from left to right and from right to left.
+ Example 2:
+
+ Input: x = -121
+ Output: false
+ Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ Example 3:
+
+ Input: x = 10
+ Output: false
+ Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        return String(x).reversed() == Array(String(x))
+    }
+}
