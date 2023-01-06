e9b4cd3238bf23fd4048ea941d2453f7b49a969b
Anzhelika Sokolova <sokolang.developer@gmail.com>
2023-01-06 14:38:57 +0200

    Add files via upload

---
+++ 1929. Concatenation of Array.swift
@@ -0,0 +1,39 @@
+//
+//  1929. Concatenation of Array.swift
+//  
+//
+//  Created by Anzhelika Sokolova on 06.01.2023.
+//
+
+/*
+ Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+ Specifically, ans is the concatenation of two nums arrays.
+
+ Return the array ans.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,1]
+ Output: [1,2,1,1,2,1]
+ Explanation: The array ans is formed as follows:
+ - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+ - ans = [1,2,1,1,2,1]
+ Example 2:
+
+ Input: nums = [1,3,2,1]
+ Output: [1,3,2,1,1,3,2,1]
+ Explanation: The array ans is formed as follows:
+ - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+ - ans = [1,3,2,1,1,3,2,1]
+ */
+
+import Foundation
+
+class Solution {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        return nums+nums
+    }
+}
